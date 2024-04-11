d81a16976c432de08a0c4d0ec3310be897a8eca9
Eduardo Secco <dudssecco.code@gmail.com>
2024-04-11 13:47:02 -0300

    Uptdate

---
+++ src/script/script.js
@@ -0,0 +1,63 @@
+const op1 = document.getElementById('op-1')
+const clo1 = document.getElementById('clo-1')
+const an1 = document.getElementById('an-1')
+
+function Open1(){
+    op1.style.display = 'none'
+    clo1.style.display = 'block'
+    an1.style.display = 'block'
+}
+
+function Close1(){
+    op1.style.display = 'block'
+    clo1.style.display = 'none'
+    an1.style.display = 'none'
+}
+
+const op2 = document.getElementById('op-2')
+const clo2 = document.getElementById('clo-2')
+const an2 = document.getElementById('an-2')
+
+function Open2(){
+    op2.style.display = 'none'
+    clo2.style.display = 'block'
+    an2.style.display = 'block'
+}
+
+function Close2(){
+    op2.style.display = 'block'
+    clo2.style.display = 'none'
+    an2.style.display = 'none'
+}
+
+const op3 = document.getElementById('op-3')
+const clo3 = document.getElementById('clo-3')
+const an3 = document.getElementById('an-3')
+
+function Open3(){
+    op3.style.display = 'none'
+    clo3.style.display = 'block'
+    an3.style.display = 'block'
+}
+
+function Close3(){
+    op3.style.display = 'block'
+    clo3.style.display = 'none'
+    an3.style.display = 'none'
+}
+
+const op4 = document.getElementById('op-4')
+const clo4 = document.getElementById('clo-4')
+const an4 = document.getElementById('an-4')
+
+function Open4(){
+    op4.style.display = 'none'
+    clo4.style.display = 'block'
+    an4.style.display = 'block'
+}
+
+function Close4(){
+    op4.style.display = 'block'
+    clo4.style.display = 'none'
+    an4.style.display = 'none'
+}
